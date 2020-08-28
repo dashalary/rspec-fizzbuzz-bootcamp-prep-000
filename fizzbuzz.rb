58a89b96b3daacaad98a84bc83569a9ef2eb01ce
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-28 19:50:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  fizz_3 = fizzbuzz(3)
   if int % 3 == 0
     print "Fizz"
 end
